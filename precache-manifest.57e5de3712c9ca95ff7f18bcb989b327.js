ae8592205995222ab58e8cfea3a51adb21b7ebe7
m00m <mgaat.hkl@gmail.com>
2020-05-27 23:49:02 +0800

    Updates

---
+++ precache-manifest.57e5de3712c9ca95ff7f18bcb989b327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d874358848d0bcdc3f2763469913770",
+    "url": "/lucky-blockchain/index.html"
+  },
+  {
+    "revision": "a92049366e5148427462",
+    "url": "/lucky-blockchain/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "111e47580c8d6719e73a",
+    "url": "/lucky-blockchain/static/js/2.43414772.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/lucky-blockchain/static/js/2.43414772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92049366e5148427462",
+    "url": "/lucky-blockchain/static/js/main.7e8155fc.chunk.js"
+  },
+  {
+    "revision": "252ea3780c0bab6b647e",
+    "url": "/lucky-blockchain/static/js/runtime-main.0117a651.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lucky-blockchain/static/media/logo.5d5d9eef.svg"
+  }
+]);
